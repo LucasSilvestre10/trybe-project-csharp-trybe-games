72be84704f9cbbbf3f02c6aed8ef06833a441857
Lucas Silvestre <lucas_silvestre.10@hotmail.com>
2023-08-28 00:36:48 -0300

    requisito 4 feito

--- src/TrybeGames/Database/TrybeGamesDatabase.cs
+++ src/TrybeGames/Database/TrybeGamesDatabase.cs
@@ -11,8 +11,10 @@ public class TrybeGamesDatabase
     // 4. Crie a funcionalidade de buscar jogos desenvolvidos por um estúdio de jogos
     public List<Game> GetGamesDevelopedBy(GameStudio gameStudio)
     {
-        // implementar
-        throw new NotImplementedException();
+         var developedGames = Games
+        .Where(game => game.DeveloperStudio == gameStudio.Id)
+        .ToList();
+        return developedGames;
     }
 
     // 5. Crie a funcionalidade de buscar jogos jogados por uma pessoa jogadora
