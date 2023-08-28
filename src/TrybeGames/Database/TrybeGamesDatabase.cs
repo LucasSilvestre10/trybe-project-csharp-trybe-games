05d729ff03063ecc112ff8c67b790dd461c195b6
Lucas Silvestre <lucas_silvestre.10@hotmail.com>
2023-08-28 00:39:47 -0300

    requisito 6 feito

--- src/TrybeGames/Database/TrybeGamesDatabase.cs
+++ src/TrybeGames/Database/TrybeGamesDatabase.cs
@@ -30,8 +30,10 @@ public class TrybeGamesDatabase
     // 6. Crie a funcionalidade de buscar jogos comprados por uma pessoa jogadora
     public List<Game> GetGamesOwnedBy(Player playerEntry)
     {
-        // Implementar
-        throw new NotImplementedException();
+        var games = from Game in Games
+                    where playerEntry.GamesOwned.Contains(Game.Id)
+                    select Game;
+        return games.ToList();
     }
 
 
